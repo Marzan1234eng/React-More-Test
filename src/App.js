92e092c13a4143ba28d4d6f765fa01057173987c
Marzan1234eng <mdmarzanhossain@gmail.com>
2021-08-23 18:53:00 +0600

    component call with object map

--- src/App.js
+++ src/App.js
@@ -2,24 +2,58 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+    /*const shopJson(){
+
+    }*/
+
+    const shopKepperDetails = [
+        {
+            name: "Arnald Bajigar",
+            position: "Manager",
+            salary: "$7500"
+        },
+        {
+            name: "Febrigous Quarter",
+            position: "Assistant Manager",
+            salary: "$6900"
+        },
+        {
+            name: "Robert Duogo",
+            position: "Executive",
+            salary: "$3500"
+        }
+    ];
   return (
     <div className="App">
       <header className="App-header">
+          {
+              shopKepperDetails.map(data => <Shop name={data.name} position={data.position}
+                                                  salary={data.salary}></Shop>)
+          }
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
 
+function Shop(props){
+    const style = {
+        color: "white",
+        border: "1px solid Green",
+        margin: "20px",
+        width: "200px",
+        height: "200px",
+        padding: "10px"
+    }
+    const {name, position, salary} = props;
+    return(
+        <div style={style}>
+            <h4>{name}</h4>
+            <h5>Position is {position}</h5>
+            <h5>Salary is {salary}</h5>
+        </div>
+    )
+
+}
 export default App;
